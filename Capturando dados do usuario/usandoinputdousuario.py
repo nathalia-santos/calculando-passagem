72ed08966cfa218f877d3af7fd6cfcfe0a224b6f
Nathalia <nathaliasantospbg@hotmail.com>
2022-02-07 16:00:42 -0300

    Exercício 2

---
+++ Capturando dados do usuario/usandoinputdousuario.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#Depois imprima a soma desses números
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print(f'Os números digitados foram {n1} e {n2}')
+print(f'O resultado da soma entre eles é {n1 + n2}')
